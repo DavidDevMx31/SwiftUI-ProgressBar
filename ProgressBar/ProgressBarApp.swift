96f81b3d2a7e24db131d50d26d006a5afbbd1b28
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2021-03-02 15:39:14 -0600

    Commit inicial

---
+++ ProgressBar/ProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressBarApp.swift
+//  ProgressBar
+//
+//  Created by David Ali on 02/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
