c031faf941dc3edd2a65b39196b6f56ffa17da2d
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2021-03-02 17:34:44 -0600

    Agrega controles para probar la progress bar

--- ProgressBar/ContentView.swift
+++ ProgressBar/ContentView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var percentage = 0.0
+    
     var body: some View {
-        Text("Hello SwiftUI!")
+        VStack {
+            CircularBarView(percentage: percentage)
+            Spacer()
+            Button(action: {
+                guard percentage < 100 else { return }
+                percentage += 10
+            }) {
+                HStack(spacing: 10) {
+                    Image(systemName: "plus.circle.fill")
+                        .foregroundColor(.blue)
+                        .frame(width: 60, height: 60)
+                    Text("Add 10% progress")
+                        .font(.body)
+                }
+            }
+        }
     }
 }
 
